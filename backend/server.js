8a7c678a411c461c16d0b39192287b96434e6149
SaqiiCoditz <saqiicoditz@gmail.com>
2025-06-22 15:26:15 +0500

    uploading #5

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,7 @@ app.get('/', (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`✅ Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
